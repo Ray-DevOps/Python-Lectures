56c292b2a52c81b058f67ee19ce8e2cc91bbe93a
RaymondAnya <94065430+Ray-DevOps@users.noreply.github.com>
2023-10-18 19:38:29 -0400

    Update 4-class-variables.py

--- OOP/4-class-variables.py
+++ OOP/4-class-variables.py
@@ -7,6 +7,34 @@
 # - Class variables are those shared by all instances of the class
 # - Class variables are the same for each instance. e.g. bonus rate
 
+# Though class attributes are set at the class level, they can also be accessed at the instance level.
+
+class Item:
+
+    discount_rate = 0.2                                              # We create a class attribute
+
+    def __init__(self, name: str, price: float, quantity=0):
+
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+
+item1 = Item("Phone", -100, -1)
+
+item2 = Item("Laptop", -1000, -3)
+
+
+print(Item.discount_rate)          ---------->  0.2                # Attribute (discount_rate) is accessible from the class
+print(item1.discount_rate)         ---------->  0.2                # Attribute (discount_rate) is also accessible from the the instance (object) item1
+print(item2.discount_rate)         ---------->  0.2                # Attribute (discount_rate) is also accessible from the the instance (object) item2
+
+
+############################################################################################
+
 
 class Employee:
     def __init__(self, first_name, last_name, pay):
