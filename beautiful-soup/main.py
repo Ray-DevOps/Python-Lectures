bfd7083e3f0223be01af8669c1ad186a98c0ede8
RaymondAnya <94065430+Ray-DevOps@users.noreply.github.com>
2023-11-11 10:07:16 -0500

    Update main.py

--- beautiful-soup/main.py
+++ beautiful-soup/main.py
@@ -29,9 +29,72 @@ print(soup.title.name)         ---------->           title                      
 print(soup)                    ---------->           # This will print out the entire HTML code
 print(soup.prettify())         ---------->           # This will print out the entire HTML code with the proper indentation to make it easier to read
 
-print(soup.p)                  ---------->           # This will print out the first paragraph in the HTML code
-print(soup.a)                  ---------->           # This will print out the first anchor tag in the HTML code
-print(soup.li)                 ---------->           # This will print out the first paragraph in the HTML code
-print(soup.ol)                 ---------->           # This will print out the first ordered list in the HTML code
-print(soup.ul)                 ---------->           # This will print out the first unordered list in the HTML code
+print(soup.p)                      ---------->           # This will print out the first paragraph in the HTML code
+print(soup.find_all(name="p"))     ---------->           # This will print out all the paragraphs in the HTML code
+
+print(soup.a)                      ---------->           # This will print out the first anchor tag in the HTML code
+print(soup.find_all(name="a"))     ---------->           # This will print out all the anchor tags in the HTML code
+
+print(soup.li)                     ---------->           # This will print out the first paragraph in the HTML code
+print(soup.ol)                     ---------->           # This will print out the first ordered list in the HTML code
+print(soup.ul)                     ---------->           # This will print out the first unordered list in the HTML code
+print(soup.find_all(name="ul"))    ---------->           # This will print out all the unordered lists in the HTML code
+
+
+# Suppose you wanted to print only the texts in the anchor tags, you would loop through all the anchor tags, and use the
+# getText() method to print them out, as shown below.
+
+for tags in all_anchor_tags:
+    print(tags.getText())      
+    
+        |
+        ↓
+
+   The App Brewery
+   My Hobbies
+   Contact Me
+
+
+
+# Suppose you wanted to print only the links in the anchor tags, you would loop through all the anchor tags, and use the
+# get() method to print the "href" as the links are the href values:
+
+for tags in all_anchor_tags:
+    print(tags.get("href"))      
+    
+        |
+        ↓
+
+https://www.appbrewery.co/
+https://angelabauer.github.io/cv/hobbies.html
+https://angelabauer.github.io/cv/contact-me.html
+
+
+
+# While the find_all() method prints every element that matches a criteria, the find() method can be used to find a particular
+# element that matches a particular criteria (such as a heading with a specific name, or paragraph with specific ID)
+
+
+heading = soup.find(name="h1", id="name")
+print(heading)             ---------->        <h1 id="name">Angela Yu</h1>
+
+section_heading = soup.find(name="h3", class_="heading")                                          # Eventhough the name of the actual attribute in the HTLM file is "class",
+print(section_heading)     ---------->       <h3 class="heading">Books and Teaching</h3>       # we add _ so that it doesn't clash with the "class" keyword for creating classes in Python
+print(section_heading.getText())        ---------->       Books and Teaching                       # To print only the text contained in the heading
+print(section_heading.name)             ---------->       h3                                       # To get just the name of the tag
+print(section_heading.get("class"))     ---------->       ['heading']                              # To get the value of the "class" attribute
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
