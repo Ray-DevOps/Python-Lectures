d50f7425e8bc13352241499b47696afe0a6dbc08
RaymondAnya <94065430+Ray-DevOps@users.noreply.github.com>
2023-11-13 13:49:46 -0500

    Update scraping-live-website.py

--- beautiful-soup/scraping-live-website.py
+++ beautiful-soup/scraping-live-website.py
@@ -12,8 +12,10 @@ print(response.text)           ---------->      # This returns the complete HTML
                                                 # website and took "inspect".
 
 
-#                     ILLUSTRATION
-#               ---------------------------
+
+################################################################# ILLUSTRATION 1 ###############################################################
+#                                                            ---------------------------
+
 
 # In this illustration, we scrap a website that lists the top 100 movies of all time, and create a text file with a list of
 # the movies and titles
@@ -43,3 +45,47 @@ with open('movies.txt','w', encoding="utf-8") as file:
         file.write(f"{title}\n")            --------->        movies.txt
 
 The output is the movies.txt file [here] (https://github.com/Ray-DevOps/Python-Notes/blob/main/beautiful-soup/movies.txt)
+
+
+
+
+################################################################# ILLUSTRATION 2 ###############################################################
+#                                                            ---------------------------
+
+# In this second example, we build an application that requests the user to enter any date in the past, and then uses BeautifulSoup to scrap the 
+# the billboard top 100 to get the top 100 songs on the billboard for that particular date.
+
+
+from bs4 import BeautifulSoup
+import requests
+
+date = input("What date would you like to travel to? Enter in format YYYY-MM-DD: ")
+
+url = f"https://www.billboard.com/charts/hot-100/{date}/"
+
+response = requests.get(url)
+
+web_page = response.text
+
+soup = BeautifulSoup(web_page, "html.parser")
+
+titles = soup.find_all("div", class_="o-chart-results-list-row-container")
+
+song_list = [ ]
+artist_list = [ ]
+
+for title in titles:
+    song_title = title.find("h3", id="title-of-a-story").text
+    song_title = song_title.strip()
+    song_list.append(song_title)
+
+    singer = title.select(".c-label")[1].text
+    singer = singer.strip()
+    artist_list.append(singer)
+
+billboard_dic = dict(list(zip(artist_list, song_list)))
+
+print('Artist | Song')
+for artist, song in billboard_dic.items():
+    print(f'{artist} | {song}')
+
